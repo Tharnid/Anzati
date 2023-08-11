8acf76c950413d90d4fe1af00333e1b325b58a97
Jeff Smith <jeffmsmithdev@gmail.com>
2023-08-11 00:40:49 -0400

    installing ng bootstrap

--- the-basics/src/app/app.component.ts
+++ the-basics/src/app/app.component.ts
@@ -3,7 +3,12 @@
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: [`
+  //       h3 {
+  //         color: darkolivegreen;
+  //       s
+  // `]
 })
 export class AppComponent {
   title = 'the-basics';
--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
   import { Component } from '@angular/core';
 
 @Component({
+  // selector: [app-servers], // This is an attribute selector
+  // selector: '.app-servers', // This is a class selector
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: "./servers.component.html",
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
