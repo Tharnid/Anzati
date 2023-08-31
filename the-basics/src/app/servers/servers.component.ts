461356723a48fa00203927e74045562ceb3cc27b
Jeff Smith <jeffmsmithdev@gmail.com>
2023-08-30 23:43:57 -0400

    on to event binding

--- the-basics/src/app/servers/servers.component.ts
+++ the-basics/src/app/servers/servers.component.ts
@@ -8,5 +8,21 @@
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Test Server';
+  serverCreated = false;
+  servers = ['Test Server', 'Test Server 2'];
 
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  onCreateServer() {
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
 }
